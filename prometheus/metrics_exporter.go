3e51b0c6c758fdf03b9c3b790448dc8e98ff8112
Jesse R. Adams <jesse.r.adams@uscis.dhs.gov>
2017-08-14 11:17:15 -0700

    Gather metrics in the background with Goroutine

--- prometheus/metrics_exporter.go
+++ prometheus/metrics_exporter.go
@@ -6,21 +6,27 @@ import (
   "log"
   "os"
   "fmt"
+  "time"
+  "strconv"
   "github.com/jesseadams/kafka-connect-monitoring-tools/common"
 )
 
-func PublishKafkaConnectMetrics(hostString string, w http.ResponseWriter) {
+var ResponseString = "Initializing..."
+
+func RetrieveKafkaConnectMetrics(hostString string) string {
+  var output string
+
   connectors, err := kafka_connect.ListConnectors(hostString, true)
   if err != nil {
     fmt.Println(err)
   }
 
   connectorCount := len(connectors)
-  io.WriteString(w, "# TYPE kafka_connect_connectorcount gauge\n")
+  output += "# TYPE kafka_connect_connectorcount gauge\n"
   line := fmt.Sprintf("kafka_connect_connectorcount %.1f\n", float64(connectorCount))
-  io.WriteString(w, line)
+  output += line
 
-  io.WriteString(w, "# TYPE kafka_connect_runningtaskscount gauge\n")
+  output += "# TYPE kafka_connect_runningtaskscount gauge\n"
   for _, connector := range connectors {
       status := new(kafka_connect.KafkaConnectorStatus)
       err = kafka_connect.CheckStatus(hostString, connector, status, true)
@@ -36,22 +42,26 @@ func PublishKafkaConnectMetrics(hostString string, w http.ResponseWriter) {
         }
       }
       line := fmt.Sprintf("kafka_connect_runningtaskscount{connector=\"%s\"} %.1f\n", connector, runningTasksCount)
-      io.WriteString(w, line)
+      output += line
   }
+
+  return output
 }
 
-func PublishSchemaRegistryMetrics(hostString string, w http.ResponseWriter) {
+func RetrieveSchemaRegistryMetrics(hostString string) string {
+  var output string
+
   subjects, err := kafka_connect.ListSubjects(hostString, true)
   if err != nil {
     fmt.Println(err)
   }
 
   subjectCount := len(subjects)
-  io.WriteString(w, "# TYPE schema_registry_subjectcount gauge\n")
+  output += "# TYPE schema_registry_subjectcount gauge\n"
   line := fmt.Sprintf("schema_registry_subjectcount %.1f\n", float64(subjectCount))
-  io.WriteString(w, line)
+  output += line
 
-  io.WriteString(w, "# TYPE schema_registry_versioncount gauge\n")
+  output +=  "# TYPE schema_registry_versioncount gauge\n"
   for _, subject := range subjects {
       versions, err := kafka_connect.ListVersions(hostString, subject, true)
 
@@ -61,19 +71,44 @@ func PublishSchemaRegistryMetrics(hostString string, w http.ResponseWriter) {
 
       versionCount := len(versions)
       line := fmt.Sprintf("schema_registry_versioncount{subject=\"%s\"} %.1f\n", subject, float64(versionCount))
-      io.WriteString(w, line)
+      output += line
+  }
+
+  return output
+}
+
+func PublishPrometheusMetrics(writer http.ResponseWriter, req *http.Request) {
+  io.WriteString(writer, ResponseString)
+}
+
+func RetrievePrometheusMetrics() {
+  kafkaConnectHostString := os.Getenv("KAFKA_CONNECT_URL")
+  schemaRegistryHostString := os.Getenv("SCHEMA_REGISTRY_URL")
+  metricsRefreshRate, err := strconv.ParseInt(os.Getenv("METRICS_REFRESH_RATE"), 10, 32)
+
+  if err != nil {
+    fmt.Println("Unable to parse refresh interval from METRICS_REFRESH_RATE")
+    fmt.Println(err)
+  }
+
+  if metricsRefreshRate < 10 {
+    metricsRefreshRate = 60
+  }
+
+  fmt.Printf("Metrics Refresh Rate: %d seconds\n", metricsRefreshRate)
+  for true {
+    fmt.Println("Refreshing metrics...")
+    kafkaConnectOutput := RetrieveKafkaConnectMetrics(kafkaConnectHostString)
+    schemaRegistryOutput := RetrieveSchemaRegistryMetrics(schemaRegistryHostString)
+    ResponseString = kafkaConnectOutput + schemaRegistryOutput
+    fmt.Println("Metrics refresh complete!")
+
+    time.Sleep(time.Duration(metricsRefreshRate) * time.Second)
   }
 }
 
-func PublishPrometheusMetrics(w http.ResponseWriter, req *http.Request) {
-  kafkaConnectHostString := os.Getenv("KAFKA_CONNECT_URL")
-  schemaRegistryHostString := os.Getenv("SCHEMA_REGISTRY_URL")
-
-  PublishKafkaConnectMetrics(kafkaConnectHostString, w)
-  PublishSchemaRegistryMetrics(schemaRegistryHostString, w)
-}
-
 func main() {
+  go RetrievePrometheusMetrics()
   http.HandleFunc("/metrics", PublishPrometheusMetrics)
   log.Fatal(http.ListenAndServe(":9045", nil))
 }
